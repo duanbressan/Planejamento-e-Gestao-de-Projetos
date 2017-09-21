7f42f96d37e7f6ec5dd5ba914cff90a6b53f703b
duanbressan <duan.trabalho@hotmail.com>
2017-09-21 11:24:10 -0300

    Iniciado tafera 2, criar funções js para carregar as matérias

---
+++ Projeto/gradeNoturno.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    var url = "https://script.google.com/macros/s/AKfycbwVobbz9rduw2NelcE5fLrT5zce9WdDseCeOiwJhLWnAGQuqsfK/exec?action=readNoturno";
+
+    $.getJSON(url, function (json) {
+
+        var divContainer = document.getElementById("showPosts");
+        
+            console.log(json.records.length); 
+
+        for (var i = 0; i < json.records.length; i++) {
+
+            console.log(json.records[i].semestre_1); 
+
+            var post = document.createElement("div");
+
+            post.innerHTML =    "<div class='content-text'>" +
+                                    "<p>" + json.records[i].semestre_1 + "</p>" +
+                                    "<p>" + json.records[i].descricao_1 + "</p>" +
+                                "</div>";
+
+            divContainer.appendChild(post);
+        }
+    });
+});  
